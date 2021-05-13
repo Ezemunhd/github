6e083eaf3c0a6a021e3062b9330f1064794ac4fd
Ezemunhd <84113145+Ezemunhd@users.noreply.github.com>
2021-05-13 18:30:53 -0300

    Update index.php

--- index.php
+++ index.php
@@ -2,18 +2,17 @@
 require_once("index.html");
 
 $Nombre = "Jose luis";
-$Nombre2 = "Curso de Desarrollo Web";
-echo "<html>
-<head>  
- 
-    <meta charset = \"utf-8\">
 
-</head>";
-echo "<body>";
-echo "<p>Mi nombre es $Nombre</p>";
-echo "<p>$Nombre es el profesor del $Nombre2</p>";
-echo "</body>";
-echo "</html>";
+if ($Nombre == "Rob")
+{
+	echo "<h1>Bienvenido Rob</h1>"
+}
+else
+{
+	
+	echo "<h1>Lo sentimos $Nombre .Usted no tiene  acceso al  sitema.</h1>"
+	
+}
 
 
 ?>
