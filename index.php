10a47a83758f95803dc8610b29476817b2db0b60
Ezemunhd <84113145+Ezemunhd@users.noreply.github.com>
2021-05-13 18:32:53 -0300

    Update index.php

--- index.php
+++ index.php
@@ -5,12 +5,12 @@ $Nombre = "Jose luis";
 
 if ($Nombre == "Rob")
 {
-	echo "<h1>Bienvenido Rob</h1>"
+	echo "<h1>Bienvenido Rob</h1>";
 }
 else
 {
 	
-	echo "<h1>Lo sentimos $Nombre .Usted no tiene  acceso al  sitema.</h1>"
+	echo "<h1>Lo sentimos $Nombre .Usted no tiene  acceso al  sitema.</h1>";
 	
 }
 
