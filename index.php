16d5adb62a8939e9c804228b50dfcf476c06df3d
Ezemunhd <84113145+Ezemunhd@users.noreply.github.com>
2021-05-13 11:38:09 -0300

    Index

--- index.php
+++ index.php
@@ -1,10 +1,18 @@
 <?php  
 require_once("index.html");
 
-
 $Nombre = "Jose luis";
-echo "Mi nombre es $Nombre";
+echo "<html>
+<head>  
+ 
+    <meta charset = \"utf-8\">
 
+</head>";
+echo "<body>";
+echo " <p>Mi nombre es $Nombre</p>";
+echo "<p>Esto es una locura</p>";
+echo "</body>";
+echo "</html>";
 
 
 ?>
