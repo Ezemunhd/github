da352a53a4da4ce1d4c67f832b4fbd87b709f2c5
Ezemunhd <84113145+Ezemunhd@users.noreply.github.com>
2021-05-13 11:40:23 -0300

    modi

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 require_once("index.html");
 
 $Nombre = "Jose luis";
+&Nombre2 = "Curso de Desarrollo Web";
 echo "<html>
 <head>  
  
@@ -10,7 +11,7 @@ echo "<html>
 </head>";
 echo "<body>";
 echo " <p>Mi nombre es $Nombre</p>";
-echo "<p>Esto es una locura</p>";
+echo "<p>$Nombre es el profesor del $Nombre2</p>";
 echo "</body>";
 echo "</html>";
 
