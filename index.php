911dfe311415bb3f36061f01018bcc8dc2845928
Ezemunhd <84113145+Ezemunhd@users.noreply.github.com>
2021-05-14 22:39:15 -0300

    Update index.php

--- index.php
+++ index.php
@@ -5,9 +5,26 @@ if ($_GET)
 	
 echo "<h1>Hola" .$_GET['Nombre']." </h1><br>";
 
+$Numero ="";
+
+echo "Numero";
+
 ?>
 
+
+
+
 <form>
+Escribe tu nombre:
 <input name="Nombre" type="text">
 <input type="submit" value="Enviar"> 
+</form>
+
+
+
+<form>
+
+<input Name="Numero" type="text">
+<input type="submit" value="Enviar">
+
 </form>
