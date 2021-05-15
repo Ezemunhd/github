1bfefed953513ec25481cdc486da86517e3a6eb4
Ezemunhd <84113145+Ezemunhd@users.noreply.github.com>
2021-05-14 23:43:44 -0300

    Update index.php

--- index.php
+++ index.php
@@ -6,8 +6,26 @@ if ($_GET)
 
 if (is_numeric ($_GET['Numero'])&& $_GET['Numero'] > 1 && $_GET['Numero']== round($_GET['Numero'],0))
 {
+	$Esprimo = true;
+	$i = 2;
 	
-	echo "<p>Excelente</p>";
+	while ($i < $_GET['Numero'] && $Esprimo)
+	{
+		
+		if ($_GET['Numero']%$i == 0)
+		{
+			
+			$Esprimo = false;
+			
+		}
+		$i++;
+		
+	}
+	if ($Esprimo)
+		echo "<p>El numero " .$_GET['Numero'] . "Es primo </p>"
+        else
+        echo  "<p>El numero".$_GET[''Numero] . "No es primo</p>"			
+	
 }
 else
 {
