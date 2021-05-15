776b86ab7a6a17d9fb625054a1fefec6a0f49113
Ezemunhd <84113145+Ezemunhd@users.noreply.github.com>
2021-05-14 23:18:51 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,10 +1,18 @@
 <?php  
 require_once("index.html");
 
+if (is_numeric ($_GET['Numero'])&& $_GET['Numero'] > 1 && $_GET['Numero']== round($_GET['Numero'],0))
+{
+	
+	echo "<p>Excelente</p>";
+}
+else
+{
+	
+	"<p>Incorrecto</p>";
+	
+}
 
-$Numero ="";
-
-echo "Numero";
 
 ?>
 
@@ -13,9 +21,9 @@ echo "Numero";
 
 
 
-
+<h1>Verificador De Numeros</h1>
 <form>
-
+<p>¡Pon un numero natural  mayor que 1!</p>
 <input Name="Numero" type="text">
 <input type="submit" value="Enviar">
 
