8b6965b2887d5524af1b704b35a9bd83e7ce1868
Ezemunhd <84113145+Ezemunhd@users.noreply.github.com>
2021-05-14 22:22:25 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,35 +1,13 @@
 <?php  
 require_once("index.html");
 
-$Nombre = "Jose luis";
-
-if ($Nombre == "Rob")
-{
-	echo "<h1>Bienvenido Rob</h1>";
-}
-else
-{
+if ($_GET)
 	
-	echo "<h1>Lo sentimos $Nombre .Usted no tiene  acceso al  sitema.</h1>";
-	
-}
-
-$Edad = "20";
-
-if ($Edad >=18)
-{
-	
-	echo "<h1>Su edad es apropiada para registrarse.</h1>";
-	
-}
-else
-{
-	
-	echo "<h1>Su edad no es apropiada para registrase.</h1>";
-	
-}
-
-
-
+echo "<h1>Hola" .$_GET['Nombre']." </h1><br>";
 
 ?>
+
+<form>
+<input name="Nombre" type="text">
+<input type="submit" value="Enviar"> 
+</form>
