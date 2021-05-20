64918a15f184763c7619c668be2d56af3a930a66
Ezemunhd <84113145+Ezemunhd@users.noreply.github.com>
2021-05-20 13:22:50 -0300

    j

--- index.php
+++ index.php
@@ -3,19 +3,5 @@ require_once("index.html");
 
 
 
-$miArray = array("Ronaldo,Martincito");
-
-print_r($miArray);
-
 ?>
 
-
-
-
-
-<form method="POST">
-<p>¿Cual es tu nombre?</p>
-<input Name="Nombre" type="text">
-<input type="submit" value="Enviar">
-
-</form>
