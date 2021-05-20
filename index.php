7592fd364c08e664d8aa98d7a2e1101ef3421913
Ezemunhd <84113145+Ezemunhd@users.noreply.github.com>
2021-05-20 19:20:52 -0300

    index

--- index.php
+++ index.php
@@ -1,10 +1,47 @@
 <?php  
 require_once("index.html");
 
+$Error ="";
+$MensajeExitoso="";
+
 if ($_POST)
+	
 {
 	
-	
+ if (!$_POST["Email"])
+ {
+	 $Error . ="Es obligatorio  una direccion de  email.<br>";	
+	 
+ }
+ 
+ if (!$_POST["Asunto"])
+ {
+	 
+	 $Error .="Es obligatorio Poner un asunto en el correo.<br>";
+	 
+ }
+ 
+ if (!$_POST["Descripcion"])
+ {
+	 
+	 $Error .="Es obligatorio agregar el contenido <br>";
+	 
+ }
+ 
+ if ($_POST["Email"] && filter_var($_POST["Email"],FILTER_VALIDATE_EMAIL)=== false)
+ {
+	 
+	 $Error .="Tu correo electronico no es valido.<br>"
+	  
+ }
+ 
+ if ($Error!="")
+{
+
+   $Error = "Hubo un error en alguno es estos campos".$Error;
+       		 
+}
+ 
 	
 }
 
