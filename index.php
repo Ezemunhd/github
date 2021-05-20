4a782c3386cc82b7dc35b91a84f05cfb86b84784
Ezemunhd <84113145+Ezemunhd@users.noreply.github.com>
2021-05-20 19:28:21 -0300

    Update index.php

--- index.php
+++ index.php
@@ -31,7 +31,7 @@ if ($_POST)
  if ($_POST["Email"] && filter_var($_POST["Email"],FILTER_VALIDATE_EMAIL)== false)
  {
 	 
-	 $Error .="Tu correo electronico no es valido.<br>"
+	 $Error .="Tu correo electronico no es valido.<br>";
 	  
  }
  
