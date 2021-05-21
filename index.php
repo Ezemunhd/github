3e83be168839cae36c533e171e154fa12f47227a
Ezemunhd <84113145+Ezemunhd@users.noreply.github.com>
2021-05-21 00:55:23 -0300

    Update index.php

--- index.php
+++ index.php
@@ -2,10 +2,8 @@
 $destinatarios = "viciconteezequiel@gmail.com";
 $Asunto = "Hola como es que estan gente";
 $Mensaje = "Esto es un prueba para ver si funcoiona.";
-$Email = "Example@hotmail.com";
+$Email = "From: Example@hotmail.com";
 
-$header = "Enviado de juegos Ps4";
-$MensajeCompleto =$Mensaje."\n Atentamente:";
 
 if (mail($destinatarios, $Asunto, $MensajeCompleto, $header))
 {
