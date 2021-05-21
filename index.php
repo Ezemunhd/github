b42674e8b1bf8d591dd3162fb42131d9b8e06626
Ezemunhd <84113145+Ezemunhd@users.noreply.github.com>
2021-05-21 20:09:34 -0300

    Update index.php

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ $mensaje = "Línea 1\r\nLínea 2\r\nLínea 3";
 // Si cualquier línea es más larga de 70 caracteres, se debería usar wordwrap()
 $mensaje = wordwrap($mensaje, 70, "\r\n");
 
-    if (mail('caffeinated@example.com', 'Mi título', $mensaje);)
+    if (mail('caffeinated@example.com', 'Mi título', $mensaje))
     {
         echo "Mensaje enviado con éxito!";
     }
