371decc100d76d384a92f2f0b6abd0f264b72db1
Ezemunhd <84113145+Ezemunhd@users.noreply.github.com>
2021-05-21 00:47:01 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,61 +1,14 @@
 <?php  
-require_once("index.html");
+$destinatarios = "hola@alexcdesign.com";
+$Asunto = "Hola como es que estan gente";
+$Mensaje = "Esto es un prueba para ver si funcoiona.";
+$Email = "Example@hotmail.com";
 
-$Error ="";
-$MensajeExitoso="";
+$header = "Enviado de juegos Ps4";
+$MensajeCompleto =$Mensaje."\nAtentamente:";
 
-if ($_POST)
-	
-{
-	
- if (!$_POST["Email"])
- {
-	 $Error .="Es obligatorio  una direccion de  email.<br>";	
-	 
- }
- 
- if (!$_POST["Asunto"])
- {
-	 
-	 $Error .="Es obligatorio Poner un asunto en el correo.<br>";
-	 
- }
- 
- if (!$_POST["Descripcion"])
- {
-	 
-	 $Error .="Es obligatorio agregar el contenido <br>";
-	 
- }
- 
- if ($_POST["Email"] && filter_var($_POST["Email"],FILTER_VALIDATE_EMAIL)== false)
- {
-	 
-	 $Error .="Tu correo electronico no es valido.<br>";
-	  
- }
- 
- if ($Error!="")
-{
-
-   $Error = "Hubo un error en alguno es estos campos".$Error;
-       			
-}
-else
-{
-$Destinatario = 'viciconteezequiel@gmail.com';
-$Subject = $_POST['Asunto'];
-$Mensaje= $_POST['Descripcion'];
-$headers = "From:".$_POST['Email'];
-
-mail($Destinatario, $Subject, $Mensaje, $headers);
-
-
-}
- 
-}
-
-
-
+mail($destinatarios, $Asunto, $MensajeCompleto, $header);
+echo "<script>alert('Correo enviado  exitosamente')</script>"
+echo "mal";
 ?>
 
