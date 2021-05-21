c9d13f9ea61bf7ca8b4da336c3d2460760a3ca95
Ezemunhd <84113145+Ezemunhd@users.noreply.github.com>
2021-05-21 20:51:18 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,15 +1,6 @@
+<?php  
+include("index.html");
 
-<?php 
-    ini_set( 'display_errors', 1 );
-    error_reporting( E_ALL );
-    $from = "prueba@solucionex.com";
-    $to = "viciconteezequiel@gmail.com";
-    $subject = "Prueba de envio de email con PHP";
-    $message = "Esto es un email de prueba enviado con PHP";
-    $headers = "From:" . $from;
-    mail($to,$subject,$message, $headers);
-	echo "Correo enviado";
-	
+
 ?>
-
 
