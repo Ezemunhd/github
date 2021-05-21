e2d4b20d3db7c1a5677b3cc2e2b8ebfee7184790
Ezemunhd <84113145+Ezemunhd@users.noreply.github.com>
2021-05-21 19:43:33 -0300

    Update index.php

--- index.php
+++ index.php
@@ -2,12 +2,12 @@
 $cabeceras  = 'MIME-Version: 1.0' . "\r\n";
 $cabeceras .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
 
-    $emailTo = "correoajoseluis@gmail.com";
+    $emailTo = "viciconteezequiel@gmail.com";
     $subject = "Curso Desarrollo Web Completo";
-    $body = "Me está encantando el curso!";
-    $headers = "From: joseluis@example.com";
+    $message = "Me está encantando el curso!";
+    $headers = "From: ezevici123@gmail.com";
 
-    if (mail($emailTo,$subject,$body,$headers))
+    if (mail($emailTo,$subject,$message,$headers))
     {
         echo "Mensaje enviado con éxito!";
     }
