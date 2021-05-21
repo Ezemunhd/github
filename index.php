119cfa4cccf053e75bda19916ed6279bee79bea3
Ezemunhd <84113145+Ezemunhd@users.noreply.github.com>
2021-05-21 20:11:52 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,18 +1,21 @@
 <?php  
-// El mensaje
-$mensaje = "Línea 1\r\nLínea 2\r\nLínea 3";
+<?php
+$para      = 'viciconteezequiel@gmail.com';
+$titulo    = 'El título';
+$mensaje   = 'Hola';
+$cabeceras = 'From: ezemunhd@gmail.com' . "\r\n" .
+    'Reply-To: webmaster@example.com' . "\r\n" .
+    'X-Mailer: PHP/' . phpversion();
 
-// Si cualquier línea es más larga de 70 caracteres, se debería usar wordwrap()
-$mensaje = wordwrap($mensaje, 70, "\r\n");
+if (mail($para, $titulo, $mensaje, $cabeceras))
+{
+echo"Perfecto";	
+}
+else
+{
+	echo"incorrecto";
+}
 
-    if (mail('caffeinated@example.com', 'Mi título', $mensaje))
-    {
-        echo "Mensaje enviado con éxito!";
-    }
-    else
-    {
-        echo "Hubo un error al enviar el mensaje";
-    }
-
+?>
 ?>
 
