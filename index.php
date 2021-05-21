59d6d67937f9991ae12bd7f9b3b1d8adabc23be5
Ezemunhd <84113145+Ezemunhd@users.noreply.github.com>
2021-05-20 23:23:23 -0300

    Update index.php

--- index.php
+++ index.php
@@ -42,10 +42,19 @@ if ($_POST)
        		 
 }
  
-	
 }
 
+$EmailTo ="viciconteezequiel@gmail.com";
+$Subject = $_POST['Asunto'];
+$body = $_POST['Descripcion'];
+$headers = $_POST['Email'];
 
+if (mail($EmailTo,$Subject,$body,$headers))
+{
+	
+	$MensajeExitoso = "Su mensaje fue enviado con exito";
+	
+}
 
 ?>
 
