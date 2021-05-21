17c1515adee2e825e384ad926e18488e05c61890
Ezemunhd <84113145+Ezemunhd@users.noreply.github.com>
2021-05-21 00:48:25 -0300

    Update index.php

--- index.php
+++ index.php
@@ -5,10 +5,10 @@ $Mensaje = "Esto es un prueba para ver si funcoiona.";
 $Email = "Example@hotmail.com";
 
 $header = "Enviado de juegos Ps4";
-$MensajeCompleto =$Mensaje."\nAtentamente:";
+$MensajeCompleto =$Mensaje."\n Atentamente:";
 
 mail($destinatarios, $Asunto, $MensajeCompleto, $header);
-echo "<script>alert('Correo enviado  exitosamente')</script>"
+echo "enviado correcto";
 echo "mal";
 ?>
 
