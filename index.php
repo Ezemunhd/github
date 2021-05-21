cf5071b302a1c369bfba5337166800bc91c90163
Ezemunhd <84113145+Ezemunhd@users.noreply.github.com>
2021-05-21 19:36:35 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,49 +1,5 @@
 <?php  
-require_once("index.html");
-
-$Error ="";
-$MensajeExitoso="";
-
-if ($_POST)
-	
-{
-	
- if (!$_POST["Email"])
- {
-	 $Error .="Es obligatorio  una direccion de  email.<br>";	
-	 
- }
- 
- if (!$_POST["Asunto"])
- {
-	 
-	 $Error .="Es obligatorio Poner un asunto en el correo.<br>";
-	 
- }
- 
- if (!$_POST["Descripcion"])
- {
-	 
-	 $Error .="Es obligatorio agregar el contenido <br>";
-	 
- }
- 
- if ($_POST["Email"] && filter_var($_POST["Email"],FILTER_VALIDATE_EMAIL)== false)
- {
-	 
-	 $Error .="Tu correo electronico no es valido.<br>";
-	  
- }
- 
- if ($Error!="")
-{
-
-   $Error = "Hubo un error en alguno es estos campos".$Error;
-       			
-}
-else
-{
-
+header("Content-type: text/html;charset=\"utf-8\"");
     $emailTo = "correoajoseluis@gmail.com";
     $subject = "Curso Desarrollo Web Completo";
     $body = "Me está encantando el curso!";
@@ -57,12 +13,6 @@ else
     {
         echo "Hubo un error al enviar el mensaje";
     }
-
-}
- 
-}
-
-
 
 ?>
 
