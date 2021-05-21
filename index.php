0d739902e9756258616f561a80727bc3c463e4c8
Ezemunhd <84113145+Ezemunhd@users.noreply.github.com>
2021-05-20 23:58:48 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,11 +1,22 @@
 <?php  
+    $EmailTo = "viciconteezequiel@gmail.com";
+    $subject = "Curso Desarrollo Web Completo";
+    $body = "Me está encantando el curso!";
+    $headers = "From: joseluis@example.com";
 
-$EmailTo = "viciconteezequiel@gmail.com";
-$subject = "hola mundo";
-$body = "Que tal andangente";
-$headers = "From: ezemunhd@gmail.com";
+    if (mail($EmailTo,$subject,$body,$headers))
+    {
+        echo "Mensaje enviado con éxito!";
+    }
+    else
+    {
+        echo "Hubo un error al enviar el mensaje";
+    }
+    
+?>
 
-if (mail ($EmailTo,$subject,$body,$headers))
+
+
 {
 	
 	echo "mensaje enviado";
@@ -21,4 +32,7 @@ else
 
 
 ?>
+
+
+
 
