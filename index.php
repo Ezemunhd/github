65423bbd209fb38c5bc3430b21068e87ae576ba5
Ezemunhd <84113145+Ezemunhd@users.noreply.github.com>
2021-05-24 21:01:36 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,11 @@
 <?php  
 include("index.html");
 
+$PaginaTiempo =  file_get_contents("https://www.weather-forecast.com/locations/Villa-Rosario/forecasts/latest");
+
+echo "$PaginaTiempo";
+
+
 
 ?>
 
