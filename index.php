e3053aa89d8d30eb6de59b223e31704c5d81aa3d
Ezemunhd <84113145+Ezemunhd@users.noreply.github.com>
2021-05-24 21:02:57 -0300

    Update index.php

--- index.php
+++ index.php
@@ -2,8 +2,9 @@
 include("index.html");
 
 $PaginaTiempo =  file_get_contents("https://www.weather-forecast.com/locations/Villa-Rosario/forecasts/latest");
+$Array1 = explode ('</h1><div class="main-title__weather-icon show-for-medium-up"><div class="weather-wrapper"><img alt="Villa Rosario Weather Forecast" src="https://www.weather-forecast.com/assets/wxvicons/daymodrain-b3970e7ed59aebac3a70cdb4a2bff38b.svg",$PaginaTiempo />');
 
-echo "$PaginaTiempo";
+echo "$Array1";
 
 
 
