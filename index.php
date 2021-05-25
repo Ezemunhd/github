8428cd76b8c277b1b24905246c6f4bab94f27fc2
Ezemunhd <84113145+Ezemunhd@users.noreply.github.com>
2021-05-24 21:11:10 -0300

    Update index.php

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ include("index.html");
 $PaginaTiempo =  file_get_contents("https://www.weather-forecast.com/locations/Villa-Rosario/forecasts/latest");
 $Array1 = explode ('</div></div></div></div><div class="row"><div class="main-title__container column"><div class="main-title__issued show-for-medium-up">Villa Rosario (Panamá, Panama) forecast issued: <time datetime="2021-05-24T17:00:00Z">12&thinsp;pm Mon 24 May 2021</time> local time <section class="next-update-countdown"><span data-countdown=""><span class="time-estimate countdown-hidden"><div><span class="countdown-delimiter">|</span> Updates in:&nbsp;<span class="hours-part"><span class="hours"></span>hr&nbsp;</span><span class="minutes">',$PaginaTiempo);
 $Array2 = explode '<div class="bn-placeholder bn-placeholder-0" id="cont_bn_by_forecast">',$PaginaTiempo);
-echo $Array1[1];
+echo $Array2[0];
 
 
 
