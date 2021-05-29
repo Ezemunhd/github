ff123068298e667e4de06b8976fd2af7ff7733b3
Ezemunhd <84113145+Ezemunhd@users.noreply.github.com>
2021-05-29 12:09:46 -0300

    Update index.php

--- index.php
+++ index.php
@@ -3,7 +3,7 @@ include("index.html");
 
 $PaginaTiempo =  file_get_contents("https://www.weather-forecast.com/locations/Villa-Rosario/forecasts/latest");
 $Array1 = explode ('<div class="main-title__issued show-for-medium-up">Villa Rosario (Panamá, Panama) forecast issued: ',$PaginaTiempo);
-$Array2 = explode ('<div class="bn-placeholder bn-placeholder-0" id="cont_bn_by_forecast">',$Array1);
+
 echo $Array2[0];
 
 
