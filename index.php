8e665ad1c9f2eb3f5da8315ea3cc3399dafafd99
Ezemunhd <84113145+Ezemunhd@users.noreply.github.com>
2021-05-29 14:41:52 -0300

    Update index.php

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ $PrevicionTiempo = "";
 
 $PaginaTiempo =  file_get_contents("https://www.weather-forecast.com/locations/Villa-Rosario/forecasts/latest");
 
-$Array1 = explode ('Villa Rosario Weather Forecast. Providing a local hourly Villa Rosario weather forecast of rain, sun, wind, humidity and temperature. <div class="read-more-small">',$PaginaTiempo);
+$Array1 = explode ('<td class="b-forecast__table-description-cell--js" colspan="9"><div class="b-forecast__table-description-title">',$PaginaTiempo);
 
 $Array2= explode ('>Read More</a></div></div></div></div></section><section class="row expanded">',$Array1[1]);
 
