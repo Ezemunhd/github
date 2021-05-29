632671417650c02be43c5055f07f0037aa427260
Ezemunhd <84113145+Ezemunhd@users.noreply.github.com>
2021-05-29 13:00:37 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php  
-include("index.html");
+
 
 $PrevicionTiempo = "";
 
@@ -13,6 +13,51 @@ $PrevicionTiempo =  $Array2[0];
 
 echo $PrevicionTiempo;
 
+?>
+
+<html>
+
+
+<head>
+<meta charset="utf-8">
+<title>¿Que tiempo hace?</title>
+<script src="jQuery.js"> </script>
+
+<link rel="stylesheet" type="text/css" href="Index.css">
+
+</head>
+
+
+
+<body background="Imagenes/Nubes.png">
+
+<form id="ClimaContainer">
+ 
+<span id="TiempoContainer"><h1>¿Que Tiempo hace?</h1> 
+<p>Introduce el Nombre de tu ciudad:</p></span>
+
+<input id="InputCiudad" placeholder="Por ej.Villa rosa"></input><br>
+<span id="BotonContainer"><input id="Boton" type="submit"></input></span>
+
+</form>
+
+<div id="PrevicionTiempo">
+ <?php
+
+
+echo $PrevicionTiempo;
 
 ?>
+</div>
+
+
+
+<script type="text/javascript">
+
+
+ </script>
+
+</body>
+
+</html>
 
