199f9a6664b96720b265a52708c929eddcde9386
Ezemunhd <84113145+Ezemunhd@users.noreply.github.com>
2021-05-29 15:57:06 -0300

    a

--- index.php
+++ index.php
@@ -109,7 +109,7 @@ h1
 <span id="TiempoContainer"><h1>¿Que Tiempo hace?</h1> 
 <p id="NombreCiud">Introduce el Nombre de tu ciudad:</p></span>
 
-<input id="InputCiudad" placeholder="Por ej.Villa rosa"></input><br>
+<input type="text" name="InputCiudad" id="InputCiudad" placeholder="Por ej.Villa rosa"></input><br>
 <span id="BotonContainer"><input id="Boton" type="submit"></input></span>
 
 </form>
