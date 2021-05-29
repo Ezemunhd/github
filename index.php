7d9fcdf53ce2cc936e2264c7c9d0794ee6a8613e
Ezemunhd <84113145+Ezemunhd@users.noreply.github.com>
2021-05-29 11:37:55 -0300

    Update index.php

--- index.php
+++ index.php
@@ -3,7 +3,7 @@ include("index.html");
 
 $PaginaTiempo =  file_get_contents("https://www.weather-forecast.com/locations/Villa-Rosario/forecasts/latest");
 $Array1 = explode ('conditions for local outdoor activities.<p class="large-loc"><b>Villa Rosario</b>',$PaginaTiempo);
-echo $Array1[1];
+echo $Array1[0];
 
 
 
