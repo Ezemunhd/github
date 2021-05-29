4ec92da3e34795ba1fc42ee4fc03438fff45d19e
Ezemunhd <84113145+Ezemunhd@users.noreply.github.com>
2021-05-29 12:24:09 -0300

    s

--- index.php
+++ index.php
@@ -3,7 +3,7 @@ include("index.html");
 
 $PaginaTiempo =  file_get_contents("https://www.weather-forecast.com/locations/Villa-Rosario/forecasts/latest");
 $Array1 = explode ('<time datetime="2021-05-29T11:00:00Z">6&thinsp;am Sat 29 May 2021</time> ',$PaginaTiempo);
-$Array2= explode ('<div class="bn-placeholder bn-placeholder-0" id="cont_bn_by_forecast">',$Array1);
+$Array2= explode ('<div class="b-forecast interactive-forecast-cont"><div class="bnr">',$Array1);
 echo $Array2[0];
 
 
