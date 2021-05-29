5959ce906943787b0d4d774467c2c9a629e0536a
Ezemunhd <84113145+Ezemunhd@users.noreply.github.com>
2021-05-29 12:29:29 -0300

    ak

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 include("index.html");
 
 $PaginaTiempo =  file_get_contents("https://www.weather-forecast.com/locations/Villa-Rosario/forecasts/latest");
-$Array1 = explode ('<li><a href="/countries/Panama"><span>Panama</span>',$PaginaTiempo);
+$Array1 = explode ('<span>Villa Rosario</span></a></li> </ol></div><div class="description__text">',$PaginaTiempo);
 $Array2= explode ('<div class="b-forecast interactive-forecast-cont"><div class="bnr">',$Array1[1]);
 echo $Array2[0];
 
