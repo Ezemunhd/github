a79bbc05e32a94b4de0794fcd460edd969c61dd5
Ezemunhd <84113145+Ezemunhd@users.noreply.github.com>
2021-05-29 12:27:31 -0300

    s

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 include("index.html");
 
 $PaginaTiempo =  file_get_contents("https://www.weather-forecast.com/locations/Villa-Rosario/forecasts/latest");
-$Array1 = explode ('<time datetime="2021-05-29T11:00:00Z">6&thinsp;am Sat 29 May 2021</time> ',$PaginaTiempo);
+$Array1 = explode ('<li><a href="/countries/Panama"><span>Panama</span>',$PaginaTiempo);
 $Array2= explode ('<div class="b-forecast interactive-forecast-cont"><div class="bnr">',$Array1[1]);
 echo $Array2[0];
 
